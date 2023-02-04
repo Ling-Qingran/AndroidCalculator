ffb95cfd77534889237ee936606cd64543614aef
Zeyu Su <suz15@miamioh.edu>
2023-02-04 17:14:47 -0500

    binding update

--- app/src/main/java/com/example/androidcalculator/MainActivity.kt
+++ app/src/main/java/com/example/androidcalculator/MainActivity.kt
@@ -4,26 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.androidcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private var canAddOperation = false
     private  var canAddDecimal = true
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun numberAction(view: View) {
         if (view is Button) {
             if (view.text == ".") {
                 if (canAddDecimal) {
-                    workingTV.append(view.text)
+                    binding.workingTV.append(view.text)
                 }
                 canAddDecimal = false
             } else {
-                workingTV.append(view.text)
+                binding.workingTV.append(view.text)
             }
             canAddOperation = true
         }
@@ -31,22 +34,22 @@ class MainActivity : AppCompatActivity() {
 
     fun operationAction(view: View) {
         if (view is Button && canAddOperation) {
-            workingTV.append(view.text)
+            binding.workingTV.append(view.text)
             canAddOperation = false
             canAddDecimal = true
         }
     }
 
     fun allClearAction(view: View) {
-        workingTV.text = ""
-        resultTV.text = ""
+        binding.workingTV.text = ""
+        binding.resultTV.text = ""
     }
     fun backSpaceAction(view: View) {
-        val workingTVText = workingTV.text.toString()
+        val workingTVText = binding.workingTV.text.toString()
         if (workingTVText.isNotEmpty()) {
-            workingTV.text = workingTVText.substring(0, workingTVText.length - 1)
+            binding.workingTV.text = workingTVText.substring(0, workingTVText.length - 1)
         }
-        resultTV.text = ""
+        binding.resultTV.text = ""
     }
     fun equalsAction(view: View) {}
 
